79b42e28b7a4081f687c473eaf1c9e9c66801d72
JUSTIN-GRAPHICS\Justin <justin9663@gmail.com>
2018-05-25 00:53:13 -0700

    Updated all of the testing references that I waa using to arrays to use the Movie objects. App is working good with the data. Am now starting to build the UI for the movies and the details screens.

--- app/src/main/java/com/wjwinter/mypopularmovies/MainActivity.java
+++ app/src/main/java/com/wjwinter/mypopularmovies/MainActivity.java
@@ -3,8 +3,7 @@ package com.wjwinter.mypopularmovies;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 import com.wjwinter.mypopularmovies.data.MoviePreferences;
 import com.wjwinter.mypopularmovies.modal.Movie;
@@ -17,47 +16,44 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView movieDbUrl;
-    TextView testTextView;
-    Button clickMe;
-    String movieJson;
+    // Variable for the list of movies
+    List<Movie> myMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Get a reference to the movie url textview
-        //Set the text in the GetMovieJsonTask onPostExecute method
-        movieDbUrl = findViewById(R.id.movie_url);
-        testTextView = findViewById(R.id.test_tv);
+        // Instantiate the list for the movies
+        myMovies = new ArrayList<>();
 
-        try {
-            Movie movie = NetworkUtils.parseMovieJsonData(movieJson);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        // Call the load movie method to get the movie data
         loadMovieData();
-        //Used for debugging and getting the movie data
-//        clickMe = findViewById(R.id.getJson);
-//        clickMe.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                new GetMovieJsonTask().execute(jsonMovieDB);
-//            }
-//        });
+//        ArrayAdapter<Movie> adapter = new ArrayAdapter<>(this, )
     }
 
+    // Method used to load the data from the Async task
     private void loadMovieData() {
+
+        // String array of the possible paths that will be used to get movie data
         String[] moviePathArray = {MoviePreferences.MOVIE_DATA_STRING};
+
+        // The Async task to load the movie data
         new GetMovieJsonTask().execute(moviePathArray);
     }
 
-    //The last param is type you will return from doInBackground and its also input to onPostExecute.
-    private class GetMovieJsonTask extends AsyncTask<String[], String[], List<Movie>> {
+    /** AsyncTask used to load the data from the movie db
+    // from https://developer.android.com/reference/android/os/AsyncTask
+    // The three types used by an asynchronous task are the following:
+    //
+    //      Params, the type of the parameters sent to the task upon execution.
+    //      Progress, the type of the progress units published during the background computation.
+    //      Result, the type of the result of the background computation.
 
+    // My method params are a String array for the url params, void for the progress(currently)
+    // and a List as the return type
+     **/
+    private class GetMovieJsonTask extends AsyncTask<String[], Void, List<Movie>> {
 
         protected void onPreExecute() {
             //Enter the details of what will happen if the load takes a while
@@ -67,14 +63,18 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected ArrayList<Movie> doInBackground(String[]... params) {
+            // moviePath is the path of the url to follow
             String[] moviePath = params[0];
+            //Pass this variable to the NetworkUtils to build the url
             URL movieDbUrl = NetworkUtils.buildUrl(moviePath[0]);
 
             try {
 
+                // Get the response from the url passed in
                 String jsonMovieResponse = NetworkUtils.
                         getResponseFromHttpUrl(movieDbUrl);
 
+                // Get the moie data from the json parser using the response
                 ArrayList<Movie> jsonMovieData = MovieJsonParser
                         .getMovieStringsFromJson(MainActivity.this, jsonMovieResponse);
 
@@ -86,20 +86,14 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
 
-
         @Override
         protected void onPostExecute(List<Movie> movies) {
-//            if (movies != null) {
-//                for (Movie movieString : strings) {
-//                    movieDbUrl.append(movieString + "\n\n");
-//                }
-//            } else {
-//                movieDbUrl.setText("Nope!");
-//            }
-
-            super.onPostExecute(movies);
+            // A loop to add all the movies to the myMovie list
+            int i = 0;
+            for (Movie movie : movies) {
+                myMovies.add(i, movie);
+                i++;
+            }
         }
     }
-
-
 }
--- app/src/main/java/com/wjwinter/mypopularmovies/utilities/MovieJsonParser.java
+++ app/src/main/java/com/wjwinter/mypopularmovies/utilities/MovieJsonParser.java
@@ -19,49 +19,41 @@ public class MovieJsonParser {
     public static ArrayList<Movie> getMovieStringsFromJson(Context context, String movieJsonStr)
             throws JSONException {
 
+        // Use this to find the list of movies that is in an element called results currently
         final String MOVIE_LIST = "results";
 
         //String array to hold all of the movie data
         //Changed from string array to movie array, and that was causing errors.
-        //Changed this from an array to an array list of movies
+        //Changed this from an array to an array list of movies.
+
+        // Use an ArraList<Movie> to holdall of the movie objects from the parsed json
         ArrayList<Movie> parsedMovieData = new ArrayList<>();
 
+        //Create a jsonObject from the json string data
         JSONObject movieJsonObj = new JSONObject(movieJsonStr);
 
         //TODO: Will want to do some error checking here to see if there are any errors with the data
 
+        // Get the jsonArray out of the jsonObject using th e MOVIE_LIST constant
         JSONArray movieArray = movieJsonObj.getJSONArray(MOVIE_LIST);
 
-        //Don't need to set the size with an array list
-//        parsedMovieData = new Movie[movieArray.length()];
-
-
+        //Create a for loop to walk thru the jsonArray and add the movies to the ArrayList
         for (int i = 0; i < movieArray.length(); i++) {
 
+            // Instantiate the movie object that will be added to the ArrayList
             Movie movieDetails = new Movie();
-            int movieId;
-            String title;
-            String moviePosterURL;
-            String overview;
-            String releaseDate;
 
+            //Get each movie from the json movieArray data
             JSONObject movie = movieArray.getJSONObject(i);
 
-            movieId = movie.getInt("id");
-            title = movie.getString("title");
-            moviePosterURL = movie.getString("poster_path");
-            overview = movie.getString("overview");
-            releaseDate = movie.getString("release_date");
+            //Assign the parameters to the movie object
+            movieDetails.setMovieId(movie.getInt("id"));
+            movieDetails.setTitle(movie.getString("title"));
+            movieDetails.setMoviePosterURL(movie.getString("poster_path"));
+            movieDetails.setOverview(movie.getString("overview"));
+            movieDetails.setReleaseDate(movie.getString("release_date"));
 
-            movieDetails.setMovieId(movieId);
-            movieDetails.setTitle(title);
-            movieDetails.setMoviePosterURL(moviePosterURL);
-            movieDetails.setOverview(overview);
-            movieDetails.setReleaseDate(releaseDate);
-
-            //This is adding only the first item to the array. This is why you made a
-            // multi dimen array, so you could set different items.
-            // Perhaps creating a movie array??
+            //Add the Movie object to the ArrayList
             parsedMovieData.add(i, movieDetails);
 
         }
